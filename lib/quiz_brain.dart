d631da4c9a401432c14f88c76bc061229d9c2d4d
mikol <mikolajek.a@gmail.com>
2023-02-09 21:04:49 +0100

    final

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -23,4 +23,17 @@ class QuizBrain {
   bool getQuestionAnswer() {
     return _questionBank[_questionNumber].questionAnswer;
   }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      print('true');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
 }
